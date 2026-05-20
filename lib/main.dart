ece2f18fc691c02dba4faac970f90a44dbcf3fde
MohammadTuhin1 <muhammadtuhin.dev@gmail.com>
2026-05-20 23:15:18 +0600

    initialize Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'location_screen.dart';
 import 'maps_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
